af33f1307a2ebf83df4a89b481d2d1f39179f64c
Osiris8 <miganosiris8@gmaaail.com>
2023-06-16 16:55:37 +0200

    replace class for className

--- src/DangerAlert.js
+++ src/DangerAlert.js
@@ -1,11 +1,14 @@
 function DangerAlert() {
   return (
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
       <strong>Hi, tadaaaaamz !</strong> Veuillez vérifier les informations que
       vous avez saisies
       <button
         type="button"
-        class="btn-close"
+        className="btn-close"
         data-bs-dismiss="alert"
         aria-label="Close"
       ></button>
